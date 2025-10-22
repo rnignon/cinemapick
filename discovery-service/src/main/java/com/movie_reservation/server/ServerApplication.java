12c2cdfe6c878252aa847ea316e696feaace1832
rnignon <rnignon@naver.com>
2025-10-22 12:59:07 +0900

    add: Project Initialization

---
+++ discovery-service/src/main/java/com/movie_reservation/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.movie_reservation.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ gateway-service/src/main/java/com/movie_reservation/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.movie_reservation.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
